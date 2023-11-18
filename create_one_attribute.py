561a7f00a3d688e9e27ea1a3ab9ad50657b3e1e9
Navruzbek02Hamrayev <hamrayevnavruz12@gmail.com>
2023-11-18 15:58:35 +0500

    return name in the Person class

--- create_one_attribute.py
+++ create_one_attribute.py
@@ -1,2 +1,11 @@
 #Create a "Person" class
+class Person :
 #Create an attribute "name" in the "Person" class
+    def __init__(self,name: str) :
+        self.name = name
+    def info(self) -> str :
+        return self.name
+n = Person(
+    name = "Navruzbek"
+)
+print(n.info())
