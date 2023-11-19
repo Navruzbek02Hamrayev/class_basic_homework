c7613f83a94140de55f549988e0eb4eaea1ae8a1
Navruzbek02Hamrayev <hamrayevnavruz12@gmail.com>
2023-11-19 13:42:28 +0500

    Return Ali

--- create_one_object.py
+++ create_one_object.py
@@ -1,3 +1,11 @@
-from create_one_attribute import Person
 
+class Person :
+    def __init__(self,name: str) :
+        self.name = name
+    def info(self) -> str :
+        return self.name
+n = Person(
+    name = "Ali"
+)
 #create an object named "person" whose name is "Ali"
+print(n.info())
